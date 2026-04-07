9c2fe98a673b4870d9bceee1c4eca052ddb2104b
Manish Kumar <manishrajkumar856@gmail.com>
2026-04-07 22:42:30 +0530

    Completed

--- frontend/src/features/Chat/services/chat.api.js
+++ frontend/src/features/Chat/services/chat.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://ai-battle-arena-x.onrender.com/api'
 });
 
 export const chatAi = async ({ prompt }) => {
